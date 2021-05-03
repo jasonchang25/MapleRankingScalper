11b03bb59613942c70216361861bd8ecaf2957c9
Jason Chang <jason.chang@sizztech.com>
2021-05-03 17:06:15 +1000

    initial project commit

---
+++ MapleRankingScalper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapleRankingScalper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
